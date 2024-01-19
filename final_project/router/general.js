ace01e122262ef4d987fff3049c68c4bc7ba4543
Selva <selvaelango@users.noreply.github.com>
2024-01-19 17:08:51 +0530

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -32,7 +32,10 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  return res.send(JSON.stringify(books,null,4));
+    let myPromise = new Promise((resolve,reject) => {
+        return  res.send(JSON.stringify(books,null,4));
+    })
+   
 });
 
 // Get book details based on ISBN
